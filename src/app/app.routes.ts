5ff72c76d03ca973dbf04f34aaa7370d4f45042e
DeWitt Angevine <dewitt.angevine@gmail.com>
2024-02-08 15:05:08 -0500

    not found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,16 @@
 import { Routes } from '@angular/router';
 import { IngredientsComponent } from './ingredients/ingredients.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
     {
         path: '',
         redirectTo: 'ingredients',
         pathMatch: 'full',
-      },{
+      },
+      {
         path: 'ingredients',
         component: IngredientsComponent,
-      }
+      },
+      { path: '**', component: NotFoundComponent }
 ];
