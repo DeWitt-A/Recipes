b44de1f709bf46507a831a500657ec9c488ee965
DeWitt Angevine <dewitt.angevine@gmail.com>
2024-02-13 18:00:38 -0500

    Ingredients List

--- src/app/ingredients/ingredients.MOCK.ts
+++ src/app/ingredients/ingredients.MOCK.ts
@@ -5,7 +5,18 @@ export const MOCK_INGREDIENTS: Ingredient[] = [
         id: '1',
         desc: 'Good olive oil',
         amount: '1 tablespoon'
+    },
+    {
+        id: '2',
+        desc: 'Onions',
+        amount: '3 cups, about 3 onions'
+    },
+    {
+        id: '3',
+        desc: 'Thyme, fresh',
+        amount: '1 teaspoon, chopped'
     }
+
 ]
 
 
--- src/app/ingredients/ingredients.component.ts
+++ src/app/ingredients/ingredients.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MOCK_INGREDIENTS } from './ingredients.MOCK';
+import { Ingredient } from './ingredient';
 
 @Component({
   selector: 'app-ingredients',
   standalone: true,
-  imports: [],
+  imports: [MatListModule],
   templateUrl: './ingredients.component.html',
   styleUrl: './ingredients.component.css'
 })
 export class IngredientsComponent {
+  
+  ingredients: Ingredient[] = MOCK_INGREDIENTS;
+
+  testIngredients = [
+    {id: 0, testIngredient: 'Pepper'},
+    {id: 1, testIngredient: 'Salt'},
+    {id: 2, testIngredient: 'Paprika'}
+  ];  
+
+  users = [
+    {id: 0, name: 'Sarah'},
+    {id: 1, name: 'Amy'},
+    {id: 2, name: 'Rachel'},
+    {id: 3, name: 'Jessica'},
+    {id: 4, name: 'Poornima'},
+  ];
 
 }
