24c97ef94eed299498603b544e6966521986f508
DeWitt Angevine <dewitt.angevine@gmail.com>
2024-02-13 18:11:17 -0500

    Test

--- src/app/ingredients/ingredients.component.ts
+++ src/app/ingredients/ingredients.component.ts
@@ -14,18 +14,4 @@ export class IngredientsComponent {
   
   ingredients: Ingredient[] = MOCK_INGREDIENTS;
 
-  testIngredients = [
-    {id: 0, testIngredient: 'Pepper'},
-    {id: 1, testIngredient: 'Salt'},
-    {id: 2, testIngredient: 'Paprika'}
-  ];  
-
-  users = [
-    {id: 0, name: 'Sarah'},
-    {id: 1, name: 'Amy'},
-    {id: 2, name: 'Rachel'},
-    {id: 3, name: 'Jessica'},
-    {id: 4, name: 'Poornima'},
-  ];
-
 }
